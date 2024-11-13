0d294445c78da6be31b9aaf36ffde4ce1c292414
PabloToPamblo <pablotopamblo@gmail.com>
2024-11-13 03:01:41 -0500

    FixedDeleteBug

--- backend/routes.py
+++ backend/routes.py
@@ -94,5 +94,8 @@ def update_picture(id):
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    data = [item for item in data if item["id"] != id]
-    return jsonify({"Message": "picture not found"}), 404
+    for picture in data:
+        if picture["id"] == id:
+            data.remove(picture)
+            return "", 204
+    return {"message": "picture not found"}, 404
